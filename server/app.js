a98455363d248145db3155818b8630ea82fa6e5c
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-02-21 15:20:04 -0600

    graphql query not working, null error

--- server/app.js
+++ server/app.js
@@ -1,21 +1,37 @@
-const express = require(`express`) // import express 
-const bodyParser = require(`body-parser`) // import body-parser 
-const graphqlHttp = require('express-graphql') // import graphql to use as middleware
-const { buildSchema } = require('graphql') // import the function to build our schema
-const mongoose = require('mongoose');
+const express = require(`express`); // import express
+const bodyParser = require(`body-parser`); // import body-parser
+const graphqlHttp = require("express-graphql"); // import graphql to use as middleware
+const { buildSchema } = require("graphql"); // import the function to build our schema
 
-const app = express() // create express server
+const app = express(); // create express server
 
-app.use(bodyParser.json()) // use body-parser middleware to parse incoming json
+const shakes = []; // create an array of Shakes to store, temp until MongoDB
 
-app.use('/graphql', graphqlHttp({
+app.use(bodyParser.json()); // use body-parser middleware to parse incoming json
+
+app.use(
+  "/graphql",
+  graphqlHttp({
     schema: buildSchema(`
+        type Shake {
+            _id: ID!
+            name: String!
+            description: String!
+            creator: String!
+        }
+
+        input ShakeInput {
+            name: String!
+            description: String!
+            creator: String!
+        }
+
         type shakeQuery {
-            shakes: [String!]!
+            shakes: [Shake!]!
         }
         
         type shakeMutation {
-            createShake(text: String): String
+            createShake(shakeInput: ShakeInput): Shake
         }
 
         schema {
@@ -24,21 +40,29 @@ app.use('/graphql', graphqlHttp({
         }
     `),
     rootValue: {
-        shakes: () => {
-            return ['Berry', 'Strawberry']
-        },
-        createShake: (args) => {
-            const shakeText = args.text 
-            return shakeText
-        }
+      shakes: () => {
+        return shakes;
+      },
+      createShake: args => {
+        const shake = {
+          _id: "1234",
+          name: args.name,
+          description: args.description,
+          creator: args.creator
+        };
+
+        shakes.push(shake);
+        return shakes;
+      }
     },
     graphiql: true
-}))
+  })
+);
 
 app.get(`/`, (request, response, next) => {
-    response.send('Our app is alive!')
-})
+  response.send("Our app is alive!");
+});
 
 app.listen(5000, () => {
-    console.log("Now listening on port 5000");
-}) // setup server to run on port 5000
+  console.log("Now listening on port 5000");
+}); // setup server to run on port 5000

3ca77d698640c8da15fa9afede7902f9448c0d1d
Jason <jas0nle@yahoo.com>
2020-03-03 12:09:35 -0600

    Added ingredients input type and tested query and mutation

--- app.js
+++ app.js
@@ -5,18 +5,42 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+// Temporary data
+const ingredients = [];
+
 app.use(bodyparser.json());
 
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+        type Ingredient {
+          _id: ID!
+          name: String!
+          servingSizeAmount: Int!
+          servingSizeUnit: String!
+          carb: Int!
+          protein: Int!
+          fat: Int!
+          imageUrl: String!
+        }
+
+        input IngredientInput {
+          name: String!
+          servingSizeAmount: Int!
+          servingSizeUnit: String!
+          carb: Int!
+          protein: Int!
+          fat: Int!
+          imageUrl: String!
+        }
+
         type RootQuery {
-            ingredients: [String!]!
+            ingredients: [Ingredient!]!
         }
 
         type RootMutation{
-            createIngredient(name: String): String
+            createIngredient(ingredientInput: IngredientInput): Ingredient
         }
 
         schema {
@@ -26,11 +50,22 @@ app.use(
     `),
     rootValue: {
       ingredients: () => {
-        return ["Blueberry", "Strawberry", "Peanut Butter"];
+        return ingredients;
       },
       createIngredient: args => {
-        const ingredientName = args.name;
-        return ingredientName;
+        const ingredient = {
+          _id: Math.random().toString(),
+          name: args.ingredientInput.name,
+          servingSizeAmount: args.ingredientInput.servingSizeAmount,
+          servingSizeUnit: args.ingredientInput.servingSizeUnit,
+          carb: args.ingredientInput.carb,
+          protein: args.ingredientInput.protein,
+          fat: args.ingredientInput.fat,
+          imageUrl: args.ingredientInput.imageUrl
+        }
+        console.log(args);
+        ingredients.push(ingredient);
+        return ingredient
       }
     },
     graphiql: true
